cdc053e2d413dadb2892827107b9b324a2b9c8d5
Sameer Bhandari <sameerbhandari52@gmail.com>
2022-09-18 22:50:46 +0530

    BinarySearch.java a valure to a parameter changed

--- lectures/10-binary search/code/src/com/kunal/BinarySearch.java
+++ lectures/10-binary search/code/src/com/kunal/BinarySearch.java
@@ -4,7 +4,7 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
-        int target = 22;
+        int target = 89;
         int ans = binarySearch(arr, target);
         System.out.println(ans);
     }
